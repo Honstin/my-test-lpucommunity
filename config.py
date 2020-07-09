c31b722393f64c38412314950cc67a917b69f604
Daniel.Delorme <dan.c.delorme@gmail.com>
2020-07-09 04:18:33 -0500

    commit update

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # Set DB Env variables
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'postgresql:///postgres:postgres@localhost:5432/lpucommunity'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Destination for uploaded photos and size limit.
     UPLOADED_PHOTOS_DEST = os.path.join(basedir, 'static/images')
